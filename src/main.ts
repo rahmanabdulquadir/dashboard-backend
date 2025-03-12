5ccf48ae801366bb42abf01c916d3f623013e79a
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-03-12 12:20:08 +0600

    user module created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Admin Dashboard')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('users, courses, admins')
+  .build();
+const documentFactory = () => SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, documentFactory)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
